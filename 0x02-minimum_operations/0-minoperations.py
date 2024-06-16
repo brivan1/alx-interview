66b1f094d4b820168a5f111a901cca90e92cfded
brivan1 <bkryan.bk@gmail.com>
2024-06-16 20:47:21 +0000

    min-ops

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''
+Minimal Operations
+'''
+
+
+def minOperations(n):
+    '''
+    a script to check minimal operations to copy and paste n number of times
+    ARGS:
+        one arg n
+
+    RETURNS:
+        the operation count
+    '''
+    if not isinstance(n, int) or n < 1:
+        return 0
+
+    ops_count = 0
+    clipboard = 0
+    done = 1
+
+    while done < n:
+        if n % done == 0:
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+        else:
+            done += clipboard
+            ops_count += 1
+
+    return ops_count
